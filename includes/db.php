db91e6dad09747f05ad6fa0952b99b61526c620d
Bilal <bilalkhan891@hotmail.com>
2018-05-29 23:31:36 +0500

    first commit

---
+++ includes/db.php
@@ -0,0 +1,11 @@
+<?php 
+
+$server = 'localhost';
+$db_user = 'root';
+$db_pwd = '';
+$db_name = 'cms';
+
+
+$conn = mysqli_connect($server, $db_user, $db_pwd, $db_name);
+
+
---
+++ index.php
@@ -0,0 +1,84 @@
+<!-- db connection -->
+<?php include 'includes/db.php' ?>
+
+<!-- header included -->
+<?php include_once 'includes/header.php'?>
+
+<!-- navbar included -->
+<?php include_once 'includes/navbar.php' ?>
+
+
+   
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                <?php 
+
+                    $query = "SELECT * FROM posts";
+                    $result = mysqli_query($conn, $query);
+
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_image = $row['post_image'];
+                        $post_content = $row['post_content'];
+                      
+                        ?>
+
+
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo $post_title; ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author; ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+                <hr>
+                <img class="img-responsive" src="images/<?php $post_image; ?>" alt="">
+                <hr>
+                <p><?php echo $post_content; ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                
+
+                <hr>
+                    <?php }   ?>
+
+                <!-- Pager -->
+                <ul class="pager">
+                    <li class="previous">
+                        <a href="#">&larr; Older</a>
+                    </li>
+                    <li class="next">
+                        <a href="#">Newer &rarr;</a>
+                    </li>
+                </ul>
+
+            </div>
+            
+            <?php include_once 'includes/sidebar.php'; ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+
+
+<?php include_once('includes/footer.php'); ?>
+
+
+
